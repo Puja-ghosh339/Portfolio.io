04732c4fccc941761ff4d773fbe5d10e6b98062f
Puja-ghosh339 <gpuja6157@gmail.com>
2025-02-18 19:52:38 +0530

    Add files via upload

---
+++ js/autoTypingAnimation.js
@@ -0,0 +1,9 @@
+setTimeout(function() {
+    $('.about-info-inner').typed({
+      strings: [
+        "<span>Hello ! I'm Puja Ghosh</span>,a data enthusiast. I adore transforming numbers into stories that guide smart choices. I'm on the lookout for roles that match my skills, challenge me, and let me make a meaningful contribution. Passion for Practical Data Insights. I trun data into actionable insights using Excel, SQL, Power BI and Python. I haven't walked the typical office path, but that didn't stop me from diving headfirst into real projects proving what i can do in action. & I easily fit into teams with my adaptability and strong analytical skills, making a quick and positive impact."
+      ],
+      typeSpeed: 7,
+      contentType: 'html'
+    });
+  }, 100);
